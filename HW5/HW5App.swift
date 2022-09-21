597fb2afa2c24ec3381b7cb03f6d963e614e8946
megmgleason <marymargaretgleason@gmail.com>
2022-09-21 17:15:02 -0400

    Initial Commit

---
+++ HW5/HW5App.swift
@@ -0,0 +1,17 @@
+//
+//  HW5App.swift
+//  HW5
+//
+//  Created by MEG G on 9/21/22.
+//
+
+import SwiftUI
+
+@main
+struct HW5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
